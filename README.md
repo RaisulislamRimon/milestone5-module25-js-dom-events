91866672cc7e06f444d91028a995a6e55806071b
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-14 07:58:42 +0000

    done

--- README.md
+++ README.md
@@ -6,4 +6,18 @@
 - - onclick="console.log('Rimon')"
 - - onclick="clickFunction()"
 - 25-2 Add onclick handler directly or via javascript
-- -
+- - <button onclick="clickFunction()">Click me</button>
+
+- - <button onclick="document.body.style.backgroundColor='yellow'">
+      Make yellow
+    </button>
+
+- - <button onclick="makeRed()">Make Red</button>
+
+- - <button onclick="makeBlue()">Make blue</button>
+
+- - <button onclick="makeGreen()">Make Green</button>
+
+- - <button onclick="makeOrange()">Make Orange</button>
+
+-
--- module25-2/js/index.js
+++ module25-2/js/index.js
@@ -1,5 +1,55 @@
 // events.js
 // console.log("hello");
+
+// option 1 : directy on the button
+{
+  /* <button onclick="console.log(1)">Show result</button> */
+}
+{
+  /* <button onclick="document.body.style.backgroundColor='yellow'">
+Make yellow
+</button> */
+}
+
+// option 2 : makeFunction() and then on the button
+
+// adding onclick functionality to the buttons
 function clickFunction() {
   console.log("hello, click me");
 }
+
+function makeRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+function makeGreen() {
+  document.body.style.backgroundColor = "green";
+}
+
+function makeBlue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+function makeOrange() {
+  document.body.style.backgroundColor = "orange";
+}
+
+// onclick by id and function
+const makeTomatoBtn = document.getElementById("makeTomato");
+// console.log(makeTomato);
+// makeTomato.onclick.style.backgroundColor = "tomato";
+// makeTomatoBtn.onclick = makeTomato();
+makeTomatoBtn.onclick = makeTomato;
+
+function makeTomato() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+// option 3 : addEventListener
+
+const makePirpleBtn = document.getElementById("makePirple");
+// console.log(makePirpleBtn);
+// console.log(makePirpleBtn.innerHTML);
+makePirpleBtn.onclick = function makePirple() {
+  document.body.style.backgroundColor = "purple";
+};
